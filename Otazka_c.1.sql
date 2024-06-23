44c2891d028a6a43a2b3184b1aa5645ca18ad3c4
JitkaT <159491364+JitkaT@users.noreply.github.com>
2024-06-24 01:16:49 +0200

    Update Otazka_c.1.sql

--- Otazka_c.1.sql
+++ Otazka_c.1.sql
@@ -6,8 +6,8 @@ JOIN czechia_payroll_industry_branch cpib ON cpib.code = cp.industry_branch_code
 AND cp.payroll_year = cp2.payroll_year + 1
 WHERE cp.value_type_code = 5958 AND cp.calculation_code = 100
 GROUP BY cp.payroll_year, cpib.name
-ORDER BY cp.industry_branch_code, cp.payroll_year
-;
+ORDER BY cp.industry_branch_code, cp.payroll_year;
+
 
 SELECT cpc.industry_branch_code, cpc.name, cpc.payroll_year, cpc2.payroll_year AS previous_year, round((cpc.gross_value-cpc2.gross_value)/cpc2.gross_value * 100, 2) AS gross_value_growth,
 CASE WHEN round((cpc.gross_value-cpc2.gross_value)/cpc2.gross_value * 100, 2) > 0 THEN "increase" ELSE "decrease" END AS result_comment
