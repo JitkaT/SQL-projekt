95de6190c0dfbd92e84f881c7292e7db8d5b2937
JitkaT <159491364+JitkaT@users.noreply.github.com>
2024-06-24 00:49:24 +0200

    Add files via upload

---
+++ Otzaka_c.1.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE VIEW czechia_payroll_comp AS 
+SELECT cp.industry_branch_code, cpib.name, cp.payroll_year, round(avg(cp.value)) AS gross_value
+FROM czechia_payroll cp  
+JOIN czechia_payroll cp2 ON cp.industry_branch_code = cp2.industry_branch_code 
+JOIN czechia_payroll_industry_branch cpib ON cpib.code = cp.industry_branch_code
+AND cp.payroll_year = cp2.payroll_year + 1
+WHERE cp.value_type_code = 5958 AND cp.calculation_code = 100
+GROUP BY cp.payroll_year, cpib.name
+ORDER BY cp.industry_branch_code, cp.payroll_year
+;
+
+SELECT cpc.industry_branch_code, cpc.name, cpc.payroll_year, cpc2.payroll_year AS previous_year, round((cpc.gross_value-cpc2.gross_value)/cpc2.gross_value * 100, 2) AS gross_value_growth,
+CASE WHEN round((cpc.gross_value-cpc2.gross_value)/cpc2.gross_value * 100, 2) > 0 THEN "increase" ELSE "decrease" END AS result_comment
+FROM czechia_payroll_comp cpc 
+JOIN czechia_payroll_comp cpc2 
+ON cpc.payroll_year = cpc2.payroll_year + 1
+GROUP BY industry_branch_code, payroll_year; 
+
---
+++ Otzaka_c.2.sql
@@ -0,0 +1,25 @@
+SELECT *
+FROM czechia_price_category cpc 
+WHERE lower(name) like lower('%mléko%') or lower(name) like lower('%chléb%');
+
+SELECT min(date_from), max(date_from)
+FROM czechia_price cp; 
+
+CREATE OR REPLACE VIEW average_prices AS 
+SELECT YEAR(cp.date_from) AS date, cp.category_code, cpc.name AS product_name, cp.value, cpc.price_value, cpc.price_unit
+FROM czechia_price cp 
+JOIN czechia_price_category cpc ON cpc.code = cp.category_code 
+WHERE region_code IS NULL
+GROUP BY cp.category_code, year(cp.date_from) ;
+
+SELECT ap.category_code, ap.product_name, ap.date, round(avg(cpc.gross_value)/ap.value, 2), ap.price_unit
+FROM average_prices ap 
+JOIN czechia_payroll_comp cpc ON cpc.payroll_year = ap.date
+WHERE category_code IN (111301, 114201) AND date IN (2006, 2018)
+GROUP BY category_code, date;
+
+
+
+
+
+
