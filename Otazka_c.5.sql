ed812279a393b8ca6aea6578acaca23cfd9fc59e
JitkaT <159491364+JitkaT@users.noreply.github.com>
2024-06-24 23:37:58 +0200

    Add files via upload

---
+++ Otazka_c.5.sql
@@ -0,0 +1,10 @@
+SELECT my.country, my.YEAR, my2.YEAR AS previous_year, round((my.GDP - my2.GDP)/my2.GDP * 100,2) AS GDP_growth, tawfd.average_wages, tawfd.average_food_price
+FROM t_jitka_ticha_project_sql_secondary_final AS my
+JOIN t_jitka_ticha_project_sql_secondary_final AS my2
+ON my.country = my2.country AND my.YEAR = my2.YEAR + 1
+JOIN total_avg_wages_food_price tawfd ON tawfd.payroll_year = my.year
+WHERE my.country = 'Czech Republic'
+GROUP BY my.year;
+
+
+
