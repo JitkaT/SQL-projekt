b69890419676449e71cb0299bb92c58826871641
JitkaT <159491364+JitkaT@users.noreply.github.com>
2024-06-24 23:32:51 +0200

    Update Otazka_c.4.sql

--- Otazka_c.4.sql
+++ Otazka_c.4.sql
@@ -1,3 +1,4 @@
+CREATE OR REPLACE VIEW total_avg_wages_food_price AS 
 SELECT cpg.payroll_year, round(avg(cpg.gross_value_growth),2) AS average_wages,round(avg(pg.price_growth),2) AS average_food_price,
 CASE WHEN round(avg(pg.price_growth),2) - round(avg(cpg.gross_value_growth),2) > 10 THEN 'Food price increase is more than 10 % above the salary increase' ELSE 'Does not meet 10 % increase'
 END AS difference
