063e73b7d927b918d3160cb491b65265ae4a4200
JitkaT <159491364+JitkaT@users.noreply.github.com>
2024-06-24 23:06:53 +0200

    Add files via upload

---
+++ Otazka_c.4.sql
@@ -0,0 +1,6 @@
+SELECT cpg.payroll_year, round(avg(cpg.gross_value_growth),2) AS average_wages,round(avg(pg.price_growth),2) AS average_food_price,
+CASE WHEN round(avg(pg.price_growth),2) - round(avg(cpg.gross_value_growth),2) > 10 THEN 'Food price increase is more than 10 % above the salary increase' ELSE 'Does not meet 10 % increase'
+END AS difference
+FROM czechia_payroll_growth cpg 
+JOIN price_growth pg ON pg.date = cpg.payroll_year 
+GROUP BY cpg.payroll_year;
