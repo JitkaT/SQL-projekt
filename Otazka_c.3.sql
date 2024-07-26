6f53b20fd3f41584cc66be9507eaff6436f6222c
JitkaT <159491364+JitkaT@users.noreply.github.com>
2024-07-26 11:26:46 +0200

    Update Otazka_c.3.sql

--- Otazka_c.3.sql
+++ Otazka_c.3.sql
@@ -1,5 +1,4 @@
 CREATE OR REPLACE VIEW price_growth AS 
-SELECT ap.product_name, ap.date, ap2.date AS prev_year, round((ap.value - ap2.value)/ap2.value * 100) AS price_growth
+SELECT ap.product_name, ap.date, ap2.date AS prev_year, round((ap.value - ap2.value) / ap2.value * 100) AS price_growth
 FROM average_prices ap
-JOIN average_prices ap2 ON ap.product_name = ap2.product_name 
-AND ap.date = ap2.date +1;
+JOIN average_prices ap2 ON ap.product_name = ap2.product_name AND ap.date = ap2.date +1;
