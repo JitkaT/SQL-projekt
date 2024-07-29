7ac13f0b8933202755bf88d2d66b52b8b8f88ef1
JitkaT <159491364+JitkaT@users.noreply.github.com>
2024-07-29 21:39:47 +0200

    Update SQL_primary_final.sql

--- SQL_primary_final.sql
+++ SQL_primary_final.sql
@@ -1,4 +1,4 @@
-#Sumarizace vstupních dat do jednoho zdroje:
+#Sumarizace vstupních dat pro otázky č. 1 až 5 do jednoho zdroje:
 CREATE TABLE t_jitka_ticha_project_SQL_primary_final AS 
 SELECT *
 FROM czechia_payroll_comp AS cpc
